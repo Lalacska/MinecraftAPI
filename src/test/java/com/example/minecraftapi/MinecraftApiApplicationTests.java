86d8ff1ab9030931b168af1209a57bca210f146b
Lalacska <87847275+Lalacska@users.noreply.github.com>
2024-02-21 08:46:08 +0100

    Initial commit

---
+++ src/test/java/com/example/minecraftapi/MinecraftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minecraftapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
